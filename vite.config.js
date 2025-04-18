3351591e23ab3d118ac52b626e3413867a4bcb65
firfe <firfe163@163.com>
2025-04-18 16:01:50 +0800

    指定端口

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  devServer: { // 指定调试启动的网络端口
+  server: { // 指定调试启动的网络端口
     host: '0.0.0.0', // 监听所有网络接口
     port: 1234,      // 指定端口号
   },
